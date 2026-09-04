a8008de055e346e7b3002fe10d567b990f996ecd
souhail kamel <souhailkamel412@gmail.com>
2026-09-04 23:41:50 +0000

    feat: wire assessment flow

--- app/(client)/assessment/page.tsx
+++ app/(client)/assessment/page.tsx
@@ -1,15 +1,5 @@
-export default function AssessmentPage() {
-  return (
-    <main className="min-h-screen bg-[#F8F5F1] flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-[#171519]">
-          BARANDY Assessment
-        </h1>
+import AssessmentFlow from "@/components/assessment/AssessmentFlow";
 
-        <p className="mt-4 text-[#171519]/60">
-          Assessment route is working.
-        </p>
-      </div>
-    </main>
-  );
+export default function AssessmentPage() {
+  return <AssessmentFlow />;
 }
